29945092c0b9201b53ea3d100045b017a259962a
codewithjabali <jabaliamunga@gmail.com>
2025-09-20 12:12:08 +0300

    Update settings.py

--- goldcinema/settings.py
+++ goldcinema/settings.py
@@ -19,7 +19,7 @@ DEBUG = "False"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "goldcinemashirt.onrender.com",
+    "goldcinema.onrender.com",
 ]
 
 # -----------------------------
@@ -131,6 +131,7 @@ else:
     SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = False
     CSRF_COOKIE_SECURE = False
+
 
 
 
